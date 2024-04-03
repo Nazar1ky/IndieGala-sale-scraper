7c34045d5fa4e8ea2c2f991745e0f58da707d544
Nazar <nazar.iurchak@gmail.com>
2024-04-03 17:11:48 +0300

    Refactor, changes, bug fixes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.wait import WebDriverWait
 
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
@@ -34,57 +34,68 @@ def save_data(data, file_name):
     with open(f"data/{file_name}", "w", encoding = "utf-8") as file:
         json.dump(data, file, ensure_ascii = False, indent = 4)
 
-def get_data(captcha_cookies, pages_count):
+def get_data(captcha_cookies):
     headers['Cookie'] = f"visid_incap_255598={captcha_cookies['visid_incap_255598']}; incap_ses_1288_255598={captcha_cookies['incap_ses_1288_255598']}"
 
     data = []
+    i = 1
+    pages_count = 1
 
-    for i in range(1, pages_count):
-        response = requests.get(f"https://www.indiegala.com/games/ajax/on-sale/lowest-price/{i}", headers=headers)
+    while i <= pages_count:
+        while True: # To make possible make many attempts
+            time.sleep(random.randint(7, 12)) # To reduce load for captcha
 
-        if response.status_code != 200:
-            print(f"Error occurred on page #{i}\n{response.content}")
-            return 0
+            response = requests.get(f"https://www.indiegala.com/games/ajax/on-sale/lowest-price/{i}", headers=headers)
 
-        try:
-            html = response.json()['html']
-        except:
-            print(f"Error occurred on page #{i}\n{response.content}")
-            return 0
+            try:
+                html = response.json()['html']
+            except:
+                print(f"Error occurred on page #{i}\n{response.content}")
+                time.sleep(random.randint(10, 20))
 
-        print(f"Page #{i} successfully scraped!")
+            print(f"Page #{i} successfully scraped!")
 
-        soup = BeautifulSoup(html, "lxml")
-        
-        products = soup.find_all("div", class_ = "main-list-results-item")
+            soup = BeautifulSoup(html, "lxml")
 
-        for product in products[1:]:
-            product_info = product.find("h3", class_ = "bg-gradient-red").find("a")
-            product_title = product_info.text
-            product_url = f"https://www.indiegala.com{product_info.get("href")}"
-            product_discount = product.find("div", class_ = "main-list-results-item-discount").text
-            product_price_without_discount = product.find("div", class_ = "main-list-results-item-price-old").text
-            product_price_with_discount = product.find("div", class_ = "main-list-results-item-price-new").text
+            pages_count = int(soup.find_all("div", class_ = "page-link-cont")[-1].find("a").get("onclick").split("/")[-1].split("'")[0])
+            products = soup.find_all("div", class_ = "main-list-results-item")
 
-            data.append({
-                "product_title": product_title,
-                "product_url": product_url,
-                "product_discount": product_discount,
-                "product_price_without_discount": product_price_without_discount,
-                "product_price_with_discount": product_price_with_discount
-            })
+            for product in products:
+                product_info = product.find("h3", class_ = "bg-gradient-red").find("a")
+                product_title = product_info.text
+                product_url = f"https://www.indiegala.com{product_info.get("href")}"
+                product_discount = product.find("div", class_ = "main-list-results-item-discount").text
+                product_price_without_discount = product.find("div", class_ = "main-list-results-item-price-old").text
+                product_price_with_discount = product.find("div", class_ = "main-list-results-item-price-new").text
 
-        time.sleep(random.randint(5,10)) # To reduce load for captcha
+                data.append({
+                    "product_title": product_title,
+                    "product_url": product_url,
+                    "product_discount": product_discount,
+                    "product_price_without_discount": product_price_without_discount,
+                    "product_price_with_discount": product_price_with_discount
+                })
+            i += 1
+            break
 
     return data
 
 def bypass_captcha():
-    pages_count = None
     captcha_cookies = None
 
     try:
-        driver = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36")
+        options.add_argument("--headless=new")
+        driver = webdriver.Chrome(options=options)
+
         driver.get("https://www.indiegala.com/games/on-sale")
+
+        wait = WebDriverWait(driver, timeout=15)
+        element = driver.find_element(By.CSS_SELECTOR, "div.page-link-cont.left a")
+        wait.until(lambda d : element.is_displayed())
+
+        # driver.save_screenshot("test.png")
 
         cookies = driver.get_cookies()
         
@@ -97,24 +108,25 @@ def bypass_captcha():
             if cookie['name'] in captcha_cookies:
                 captcha_cookies[cookie['name']] = cookie['value']
 
-        pages_count = int(driver.find_elements(By.CSS_SELECTOR, "div.page-link-cont.left a")[-3].text)
-    
+    except Exception as e:
+        print(e)
+
     finally:
         driver.quit()
-        return captcha_cookies, pages_count
+        return captcha_cookies
 
 def main():
     print("Bypassing Captcha...")
     page_info = bypass_captcha()
     print("Captcha bypassed! Parsing...")
 
-    data = get_data(page_info[0], page_info[1])
+    data = get_data(page_info)
     if data == 0:
         return
     
-    file_name = datetime.datetime.now().strftime("%d_%m_%Y")
+    file_name = f"{datetime.datetime.now().strftime("%d_%m_%Y")}.json"
 
     save_data(data, file_name)
-  
+    
 if __name__ == "__main__":
     main()
---
+++ rss.py
@@ -0,0 +1,67 @@
+import os
+import json
+import datetime
+import requests
+from bs4 import BeautifulSoup
+
+def save_data(data, file_name):
+    if not os.path.exists("data_rss/"):
+        os.mkdir("data_rss")
+    
+    with open(f"data_rss/{file_name}", "w", encoding = "utf-8") as file:
+        json.dump(data, file, ensure_ascii = False, indent = 4)
+
+def get_page_count():
+    response = requests.get(f"https://www.indiegala.com/store_games_rss?sale=true")
+    soup = BeautifulSoup(response.content, "xml")
+
+    total_pages = int(soup.find("totalPages").text)
+
+    return total_pages
+
+def get_data():
+    # sale = "true" if only_sale_games else "false"
+
+    data = []
+
+    total_pages = get_page_count()
+
+    for i in range(1, total_pages + 1):
+        print(f"Scraping {i}/{total_pages}")
+
+        response = requests.get(f"https://www.indiegala.com/store_games_rss?page={i}&sale=true")
+        soup = BeautifulSoup(response.content, "xml")
+
+        products = soup.find_all("item")
+
+        for product in products:
+            product_title = product.find("title").text
+            product_url = product.find("link").text
+            product_discount = product.find("discountPercentUSD").text
+            product_price_with_discount = product.find("discountPriceUSD").text
+            product_price_without_discount = product.find("priceUSD").text
+            product_discount_end = product.find("discountEnd").text
+            blacklisted_countries = product.find("notAvailableRegions").text
+
+            data.append({
+                "product_title": product_title,
+                "product_url": product_url,
+                "product_discount": f"{float(product_discount)}%",
+                "product_price_without_discount": f"{product_price_without_discount}$",
+                "product_price_with_discount": f"{product_price_with_discount}$",
+                "product_discount_end": datetime.datetime.fromisoformat(product_discount_end).strftime("%d/%m/Y"),
+                "blacklisted_countries": blacklisted_countries
+            })
+    data.sort(key=lambda x: x["product_price_with_discount"])
+
+    return data
+
+def main():
+    data = get_data()
+
+    file_name = f"{datetime.datetime.now().strftime("%d_%m_%Y")}.json"
+
+    save_data(data, file_name)
+
+if __name__ == "__main__":
+    main()
