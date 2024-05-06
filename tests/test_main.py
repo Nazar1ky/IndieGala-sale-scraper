fc5b2be87416c69687fe64819d5a94efd423e9d6
Nazar <nazar.iurchak@gmail.com>
2024-05-06 22:26:53 +0300

    remove ruff noqa

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,11 +7,11 @@ from main import parse_page
 
 class Tests(unittest.TestCase):  # noqa: D101
     @classmethod
-    def setUpClass(self) -> None:  # noqa: ANN102
+    def setUpClass(self) -> None:
         with open("tests/page1.html", encoding="utf-8") as file:
             self.test_data = file.read()
 
-    def test_parse_page(self):  # noqa: ANN101
+    def test_parse_page(self):
         result = parse_page(self.test_data)
 
         self.assertEqual(result[0], data1[0])
